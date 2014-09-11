783ad369648594bfc5e819228c55013603f183b8
Rod Calumpong <rod@ripple.com>
2014-09-10 17:11:01 -0700

    [TASK] Refactor http calls with factory style structure and refresh balance and transaction table after confirming payment

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,10 +12,145 @@ angular.module('helloWorldPaymentsApp')
     function ($scope, $http, $q, localStorageService, RippleRest) {
     var hostname = 'http://localhost',
         port = '5990',
-        socketAddress = hostname + ':' + port,
-        baseUrl;
-    
-    var issuer = {
+        socketAddress = hostname + ':' + port + '/v1', // ripple-rest server
+        baseUrl; // set after user inputs ripple address
+
+    // user does not have to enter their ripple address again on initial page after refresh (unless there's an error)
+    var rippleAddressInStore = localStorageService.get('rippleAddress');
+    $scope.rippleAddress = rippleAddressInStore || '';
+
+    // save any changes to $rippleAddress in local storage
+    $scope.$watch('rippleAddress', function(newAddress) {
+      localStorageService.set('rippleAddress', newAddress);
+    });
+
+    // ng-show conditional
+    $scope.started = false;
+
+    // walet information tables
+    $scope.balances = [];
+    $scope.transactions = [];
+
+    // required for payment submission
+    var uuid, path;
+    $scope.paths = [];
+    $scope.payment = {
+      amount: '',
+      currency: '',
+      destination_account: '',
+      rippleSecret: ''
+    };
+
+    // start over payment process
+    $scope.cancel = function() {
+      // reset messages
+      $scope.startErrorMessage = '';
+      $scope.preparePaymentErrorMessage = '';
+      $scope.sendPaymentErrorMessage = '';
+      $scope.sendPaymentSuccessMessage = '';
+      $scope.validatePaymentErrorMessage = '';
+      $scope.validatePaymentSuccessMessage = '';
+
+      // ng-switch state
+      $scope.moneyTransferFlowState = 'preparing'
+    };
+
+    $scope.cancel();
+
+    // will not start unless given account can poll balances and transactions
+    $scope.start = function() {
+      baseUrl = socketAddress + '/accounts/' + $scope.rippleAddress;
+
+      RippleRest.getBalances(baseUrl)
+        .then(function(response) {
+          $scope.balances = response.balances;
+
+          return RippleRest.getTransactions(baseUrl);
+        })
+        .then(function(response) {
+          console.log(response);
+          $scope.started = true;
+          $scope.startErrorMessage = '';
+          $scope.transactions = response.payments;
+        })
+        .catch(function(error) {
+          $scope.startErrorMessage = error.message || error;
+          $scope.rippleAddress = '';
+        });
+    };
+
+    // set up payment process and find valid payment paths for available usable currencies
+    $scope.preparePayment = function() {
+      RippleRest.preparePayment(baseUrl, 
+                                $scope.payment.destination_account,
+                                $scope.payment.amount,
+                                $scope.payment.currency)
+        .then(function(response) {
+          $scope.preparePaymentErrorMessage = '';
+          $scope.paths = response.payments;
+          $scope.moneyTransferFlowState = 'pathChoosing';
+        })
+        .catch(function(error) {
+          $scope.preparePaymentErrorMessage = error.message || error;
+        });
+    };
+
+    // choose to fund payment with specified currency
+    $scope.choosePath = function(index) {
+      path = $scope.paths[index];
+
+      // add issuer if currency is not XRP
+      path.source_amount.issuer = RippleRest.issuerAddress[path.source_amount.currency.toUpperCase()];
+
+      $scope.moneyTransferFlowState = 'sending';
+    };
+
+    // actually submits payment to ledger
+    $scope.sendPayment = function() {
+      // generate UUID for transaction ID, unique for every transaction
+      RippleRest.getUUID(socketAddress)
+        .then(function(response) {
+          return RippleRest.submitPayment(socketAddress, path, $scope.payment.rippleSecret, response.uuid);
+        })
+        .then(function(response) {
+          var message = [$scope.payment.amount, $scope.payment.currency, 'successfully sent!'];
+            
+          $scope.sendPaymentErrorMessage = '';
+          $scope.sendPaymentSuccessMessage = message.join(' ');
+          $scope.moneyTransferFlowState = 'confirming';
+        })
+        .catch(function(error) {
+          $scope.sendPaymentErrorMessage = error.message || error;
+        });
+    };
+
+    $scope.confirmPayment = function() {
+      RippleRest.confirmPayment(baseUrl, uuid)
+        .then(function(){
+          $scope.validatePaymentSuccessMessage = 'Successfully validated';
+
+          setTimeout(function() {
+            $scope.$apply(function() {
+              $scope.validatePaymentSuccessMessage = '';
+              $scope.moneyTransferFlowState = 'preparing'; // reset payment flow
+
+              // Update balances and transactions tables
+              $scope.start();
+            });
+          }.bind(this), 10000);
+
+        })
+        .catch(function(error) {
+          $scope.validatePaymentErrorMessage = error.message || error;
+        });
+    };
+  }])
+
+  .factory('RippleRest', function RippleRest($http, $q) {
+    var rippleRest = {};
+
+    rippleRest.issuerAddress = {
+      'XRP': '',
       'AUD': 'rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B', // BitStamp
       'BTC': 'rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B', // BitStamp
       'CAD': 'rBcYpuDT1aXNo4jnqczWJTytKGdBGufsre', // WeExchange
@@ -28,188 +163,6 @@ angular.module('helloWorldPaymentsApp')
       'TRC': 'rfYv1TXnwgDDK4WQNbFALykYuEBnrR4pDX', // DividendRippler
       'USD': 'rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B', // BitStamp
     };
-
-    // user does not have to enter their ripple address more than once on initial page
-    var rippleAddressInStore = localStorageService.get('rippleAddress');
-    $scope.rippleAddress = rippleAddressInStore || '';
-
-    // save any changes to $rippleAddress in local storage
-    $scope.$watch('rippleAddress', function(newAddress) {
-      localStorageService.set('rippleAddress', newAddress);
-    });
-
-    // ng-show conditional
-    $scope.started = false;
-
-    // ng-switch state
-    $scope.moneyTransferFlowState = 'preparing'
-
-    $scope.startErrorMessage = '';
-    $scope.preparePaymentErrorMessage = '';
-    $scope.sendPaymentErrorMessage = '';
-    $scope.sendPaymentSuccessMessage = '';
-    $scope.validatePaymentErrorMessage = '';
-    $scope.validatePaymentSuccessMessage = '';
-
-    $scope.balances = [];
-    $scope.transactions = [];
-
-    // required for payment submission POST
-    var uuid, path, sendData;
-    $scope.payment = {
-      amount: '',
-      currency: '',
-      destination_account: '',
-      rippleSecret: ''
-    };
-    $scope.paths = [];
-
-    // will not start unless account has balances and transactions
-    $scope.start = function() {
-      baseUrl = socketAddress + '/v1/accounts/' + $scope.rippleAddress;
-
-      $http.get(baseUrl + '/balances')
-        .then(function(response) {
-          if (!response.data.success) {
-            return $q.reject(response);
-          } else {
-            $scope.balances = response.data.balances;
-            
-            return $http.get(baseUrl + '/payments');
-          }
-        })
-        .then(function(response) {
-          if (!response.data.success) {
-            return $q.reject(response);
-          } else {
-            $scope.started = true;
-            $scope.startErrorMessage = '';
-            $scope.transactions = response.data.payments;
-          }
-        })
-        .catch(function(error) {
-          $scope.startErrorMessage = error.data.message || error;
-          $scope.rippleAddress = '';
-        });
-    };
-
-    // start over payment process
-    $scope.cancel = function() {
-      $scope.startErrorMessage = '';
-      $scope.preparePaymentErrorMessage = '';
-      $scope.sendPaymentErrorMessage = '';
-      $scope.sendPaymentSuccessMessage = '';
-      $scope.validatePaymentErrorMessage = '';
-      $scope.validatePaymentSuccessMessage = '';
-      $scope.moneyTransferFlowState = 'preparing';
-    };
-
-    // set up payment process and find valid payment paths
-    $scope.preparePayment = function() {
-      var pathUrl = baseUrl + '/payments/paths/' + $scope.payment.destination_account + 
-                    '/' + $scope.payment.amount + '+' + $scope.payment.currency;
-      var currency = $scope.payment.currency.toUpperCase();
-
-      // add issuer if currency is not XRP
-      if (currency !== 'XRP') {
-        pathUrl += '+' + issuer[currency];
-      }
-      
-      // prepare payment and find viable paths
-      $http.get(pathUrl)
-        .then(function(response) {
-          if (!response.data.success) {
-            return $q.reject(response)
-          } else {
-            $scope.preparePaymentErrorMessage = '';
-            $scope.paths = response.data.payments;
-            $scope.moneyTransferFlowState = 'pathChoosing';
-          }
-        })
-        .catch(function(error) {
-          $scope.preparePaymentErrorMessage = error.data.message || error;
-        });
-    };
-
-    $scope.choosePath = function(index) {
-      var currency;
-
-      path = $scope.paths[index];
-      currency = path.source_amount.currency.toUpperCase();
-
-      // add issuer if currency is not XRP
-      if (currency !== 'XRP') {
-        path.source_amount.issuer = issuer[currency];
-      }
-
-      $scope.moneyTransferFlowState = 'sending';
-    };
-
-    $scope.sendPayment = function() {
-      var uuidUrl = socketAddress + '/uuid';
-      var sendUrl = socketAddress + '/payments';
-
-      // generate UUID for transaction ID, unique for every transaction
-      $http.get(uuidUrl)
-        .then(function(response) {
-          if (!response.data.success) {
-            return $q.reject(response);
-          } else {
-            uuid = response.data.uuid;
-
-            sendData = {
-              client_resource_id: uuid,
-              secret: $scope.payment.rippleSecret,
-              payment: path
-            };
-            
-            // send payment
-            return $http.post(sendUrl, sendData);
-          }
-        })
-        .then(function(response) {
-          if (!response.data.success) {
-            return $q.reject(response);
-          } else {
-            var message = [$scope.payment.amount, $scope.payment.currency, 'successfully sent!'];
-            
-            $scope.sendPaymentErrorMessage = '';
-            $scope.sendPaymentSuccessMessage = message.join(' ');
-            $scope.moneyTransferFlowState = 'confirming';
-
-            // TODO - Update balances and transactions tables after sending confirmation
-          }
-        })
-        .catch(function(error) {
-          $scope.sendPaymentErrorMessage = error.data.message || error;
-        });
-    };
-
-    $scope.confirmPayment = function() {
-      var confirmUrl = baseUrl + '/payments/' + uuid;
-
-      $http.get(confirmUrl)
-        .then(function(response) {
-          if (!response.data.success) {
-            return $q.reject(response);
-          } else {
-            $scope.validatePaymentSuccessMessage = 'Successfully validated';
-
-            setTimeout(function() {
-              $scope.$apply(function() {
-                $scope.validatePaymentSuccessMessage = '';
-                $scope.moneyTransferFlowState = 'preparing';
-              });
-            }.bind(this), 3000);
-          }
-        })
-        .catch(function(error) {
-          $scope.validatePaymentErrorMessage = error.data.message || error.data;
-        });
-    };
-  }])
-  .factory('RippleRest', function RippleRest($http, $q) {
-    var rippleRest = {};
 
     rippleRest._get = function(baseUrl, paths) {
       return $http.get(baseUrl + paths.join(''))
@@ -225,6 +178,10 @@ angular.module('helloWorldPaymentsApp')
         });
     };
 
+    rippleRest.getUUID = function(serverUrl) {
+      return this._get(serverUrl, ['/uuid']);
+    };
+
     rippleRest.getBalances = function(baseUrl) {
       return this._get(baseUrl, ['/balances']);
     };
@@ -233,16 +190,16 @@ angular.module('helloWorldPaymentsApp')
       return this._get(baseUrl, ['/payments']);
     };
 
-    rippleRest.preparePayment = function(baseUrl, destinationAccount, destinationAmount) {
-      return this._get(baseUrl, ['/payments/paths/', destinationAccount, '/', destinationAmount]);
+    rippleRest.preparePayment = function(baseUrl, destinationAccount, amount, currency) {
+      var issuer = this.issuerAddress[currency.toUpperCase()];
+
+      if (issuer) issuer = '+' + issuer;
+
+      return this._get(baseUrl, ['/payments/paths/', destinationAccount, '/', amount, '+', currency, issuer]);
     };
 
-    rippleRest.confirmPayment = function(baseUrl, hash) {
-      return this._get(baseUrl, ['/payments/', hash]);
-    };
-
-    rippleRest.submitPayment = function(server, paymentObj, secret, hash) {
-      return $http.post(server + '/v1/payments', {paymentObj: payment, secret: secret, hash: hash})
+    rippleRest.submitPayment = function(serverUrl, paymentObj, secret, hash) {
+      return $http.post(serverUrl + '/payments', {payment: paymentObj, secret: secret, client_resource_id: hash})
         .then(function(response) {
           if (response.data.success) {
             return response.data;
@@ -253,6 +210,10 @@ angular.module('helloWorldPaymentsApp')
         .catch(function(error) {
           return $q.reject(error.data);
         });
+    };
+
+    rippleRest.confirmPayment = function(baseUrl, hash) {
+      return this._get(baseUrl, ['/payments/', hash]);
     };
 
     return rippleRest;
